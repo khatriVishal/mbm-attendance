11960d2b94c150f2f29c65da6fa4bd311d71cfb1
khatriVishal <vkkhatri9563@gmail.com>
2024-04-07 23:26:13 +0530

    navigation feature

--- frontend/src/student/student_int.jsx
+++ frontend/src/student/student_int.jsx
@@ -1,6 +1,6 @@
 import Navbar from '../Home/navbar.jsx'
 import Profileicon from '../assets/profile-icon.png'
-// import Attendanceicon from '../assets/attendance.png';
+// import { useNavigate } from "react-router-dom";
 import Dashboardicon from '../assets/dashboard-logo.png';
 import Attendanceicon from '../assets/attendence-logo.png';
 import Subjecticon from '../assets/subjecticon.png';
@@ -14,16 +14,23 @@ import "./student_int.css";
 // import SideNav from './sidenav.jsx';
 function student() {
   const navigate = useNavigate();
+  let [state,setstate] = useState(1);
   // let [profile , setprofile] = useState(0);
   // let [attendance , setattendance] = usestate(0);
   const location = useLocation();
   const { data } = location.state;
   console.log(data.userData.userName);
-  function handleprofile() {
-    navigate("user", { state: { data } });
+  function handle1(){
+  setstate(1);
   }
-  function handleattendance() {
-    navigate("attendance");
+  function handle2(){
+    setstate(2);
+    // console.log("2");
+  }
+  function handle3(){
+    setstate(3);
+    navigate("/");
+    // console.log("3")
   }
   return (
     <>
@@ -31,29 +38,33 @@ function student() {
       <div className="bodyy">
         <div className="studentdashboard">
           <div className="sidenavbar">
-            <div className="nav-items">
+            <div className="nav-items" onClick={handle1}>
               <div className="nav-i2">
               <img src={Dashboardicon} alt="" />
               </div>
               <h3>Dashboard</h3>
             </div>
-            <div className="nav-items">
+            <div className="nav-items" onClick={handle2}>
               <img src={Attendanceicon} alt="" />
               <h3>Attendance</h3>
             </div>
-            <div className="nav-items">
-              <img src={Subjecticon} alt="" />
-              <h3>Subjects</h3>
-            </div>
-            <div className="nav-items">
+            
+            <div className="nav-items" onClick={handle3} >
               <img src={Logouticon} alt="" />
               <h3>Logout</h3>
             </div>
 
           </div>
           <div className="studentbodycontainer">
+            <div  className={state==1? "clicked" : "unclicked"} >
+
+            <Student_dash data={data}></Student_dash>
+            </div>
+            <div className={state==2? "clicked" : "unclicked"}>
             <Attendance></Attendance>
-                   {/* <Student_dash data={data}></Student_dash> */}
+            </div>
+           
+                   {/*  */}
           </div>
         </div>
       </div>
